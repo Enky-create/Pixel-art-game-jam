afc06cdfc9eecfa7f0d7d2125f85afc9649b4bf4
Enky-create <komkowvadim@gmail.com>
2024-05-16 08:32:31 +0300

    home port

--- Meanderer/resourses/ship_move/ship_move_resource.gd
+++ Meanderer/resourses/ship_move/ship_move_resource.gd
@@ -3,5 +3,7 @@ class_name ShipMoveResource
 @export var max_speed:float
 @export var min_speed:float
 @export var acceleration:float
-@export var rotation_speed:float
+@export var min_rotation_speed:float
+@export var max_rotation_speed:float
+@export var rotation_acceleration:float
 @export var fritction:float
--- Meanderer/scenes/complexed_modules/ship/ship.gd
+++ Meanderer/scenes/complexed_modules/ship/ship.gd
@@ -2,15 +2,18 @@ extends Entity
 class_name Ship
 @export var move_resource:ShipMoveResource
 @onready var current_speed:float = move_resource.min_speed
+@onready var current_rotation_speed:float =move_resource.min_rotation_speed
 func move(direction:Vector2,delta):
 	if(direction != Vector2.ZERO):
 		current_speed=move_toward(current_speed, move_resource.max_speed,move_resource.acceleration)
 		print("current speed is "+str(current_speed))
+		var rotation_direction=0
 		if(direction.x>0):
-			rotation_degrees+=move_resource.rotation_speed*delta
+			rotation_direction=1
 		if(direction.x<0):
-			rotation_degrees-=move_resource.rotation_speed*delta
-		
+			rotation_direction=-1
+			current_rotation_speed=lerp(current_rotation_speed,move_resource.max_rotation_speed,move_resource.rotation_acceleration)
+		rotation_degrees+=rotation_direction* current_rotation_speed *delta
 		print(direction)
 		velocity=Vector2(0,direction.y).rotated(deg_to_rad(rotation_degrees))*current_speed*delta
 	else:
